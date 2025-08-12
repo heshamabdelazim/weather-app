2c20b33e473fec0dd1b7939f71cc038c61f2f27a
hesham <heshamabdelazim3@gmail.com>
2025-08-13 00:08:11 +0300

    Hiding weather api key

--- src/components/apiStructure.ts
+++ src/components/apiStructure.ts
@@ -1,11 +1,9 @@
 // the api above has return , So the state of this slice will be a (modified API)
 // documentation of API here https://openweathermap.org/current
 
-const API_KEY = "59baed44bcb9c644463cb7d5986e0ab7&units=metric$";
-
-// const BASE_URL= "https://api.openweathermap.org/data/2.5/weather"
-const BASE_URL = "https://pro.openweathermap.org/data/2.5/weather";
-
+// const BASE_URL= "https://api.openweathermap.org/data/2.5/weather" //old
+const BASE_URL = "https://pro.openweathermap.org/data/2.5/weather"; //new
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 export const generateAPI_byCity: generateAPI_byCity = (
   city,
   exclude,
